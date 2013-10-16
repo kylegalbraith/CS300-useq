d92a01e0cfecaf0ab7ee6134874f11842a3a77de
Kyle Galbraith <kyle.galbraith459@gmail.com>
2013-10-16 11:09:17 -0700

    Better encapsulation of sequ
    Use a setup function that will return the sequ object back to main so
    that all the command line parsing and setting of sequ object defaults is
    hidden from main. Added in a check for floating points. Right now based
    on spec I am going to warn the user that they should use integers
    instead of floats and then cast the float to an int.

--- sequ.py
+++ sequ.py
@@ -2,7 +2,38 @@
 
 # Need sys to pass arguments in
 import sys
+import math
 from sequ_obj import *
+
+def setup():
+    # Create a new sequ_obj which will have all of the defaults set for normal sequ operation.
+    initialObj = sequ_obj()
+
+    # Get the arguments that have been passed in, and ignore the first one since it is the invocation of the script
+    arguments = sys.argv[1:len(sys.argv)]
+
+    # Get the total args passed in as exactly 2 are required
+    totalArgs = len(arguments)
+
+    if totalArgs > 2 or totalArgs == 1 or totalArgs == 0:
+        usage(1)
+
+    for x in range(totalArgs):
+        try:
+            arguments[x] = float(arguments[x])
+        except ValueError:
+            usage(2)
+    
+    # Warn if arguments are not integers. Is there a fractional part.
+    if not math.floor(arguments[0]) == arguments[0]:
+        usage(3)
+    if not math.floor(arguments[1]) == arguments[1]:
+        usage(3)
+
+    initialObj.startValue = int(arguments[0])
+    initialObj.endValue = int(arguments[1])
+    
+    return initialObj
 
 # usage will be the initial error handling function so if the initial requirements are not met, print out the correct thing to do.
 def usage(errorCode):
@@ -12,11 +43,13 @@ def usage(errorCode):
     elif(errorCode == 2):
         print 'ERROR - sequ CLO only accepts integer types'
         exit(1)
+    elif(errorCode == 3):
+        print 'WARNING - Start and end arguments should be integers'
 
 # outputSeq will output the sequence between the integers passed in
 def outputSeq(sequObj):
-    start = arguments[0]
-    end = arguments[1]
+    start = sequObj.startValue
+    end = sequObj.endValue
     
     # If the start is greater than the end then we just exit
     if start > end:
@@ -27,28 +60,8 @@ def outputSeq(sequObj):
     exit(0)
 
 # Create a new sequ_obj which will have all of the defaults set for normal sequ operation.
-initialObj = sequ_obj()
-
-# Get the arguments that have been passed in, and ignore the first one since it is the invocation of the script
-arguments = sys.argv[1:len(sys.argv)]
-
-# Get the total args passed in as exactly 2 are required
-totalArgs = len(arguments)
-
-if totalArgs > 2 or totalArgs == 1 or totalArgs == 0:
-    usage(1)
-
-for x in range(totalArgs):
-    try:
-        arguments[x] = int(arguments[x])
-    except ValueError:
-        usage(2)
-
-if isinstance(arguments[0], (int, long)) and isinstance(arguments[1], (int, long)):
-    outputSeq(arguments)
-
-if(initialObj.passedArgs(arguments) == true):
-    outputSeq(initialObj)
+initialObj = setup()
+outputSeq(initialObj)
 
 
 
