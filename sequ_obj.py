f7b003a93d0d1510cb81e63ed5195689a38382c7
Kyle M Galbraith <kgalb2@fiordland.cs.pdx.edu>
2013-10-17 12:37:35 -0700

    Removing useless code from sequ_obj.py as it is no longer needed since we have the setup() function. Also adding copyright code to top of file.

--- sequ_obj.py
+++ sequ_obj.py
@@ -1,16 +1,10 @@
+# # Copyright (C) 2013 by Kyle Galbraith
+
 class sequ_obj(object):
     import sys
     """description of class"""
     startValue = 0
     endValue = 0
 
-    def passedArgs(e, arguments):
-        print 'In The sequObj'
-        if isinstance(arguments[0], (int, long)) and isinstance(arguments[1], (int, long)):
-            e.startValue = arguments[0]
-            e.endValue = arguments[1]
-            return true
-        else:
-            return false
 
 
