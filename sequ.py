e899815206d6ca400c288e5433a09d1707843980
Kyle Galbraith <kyle.galbraith459@gmail.com>
2013-10-19 21:20:47 -0700

    Asserts for sequ
    Check that the expected initial values are created.
    Check that the expected data types are actually created.

--- sequ.py
+++ sequ.py
@@ -6,11 +6,15 @@
 # Need sys to pass arguments in
 import sys
 import math
+from types import *
 from sequ_obj import *
 
 def setup():
     # Create a new sequ_obj which will have all of the defaults set for normal sequ operation.
     initialObj = sequ_obj()
+
+    assert initialObj.startValue == 0, "Start value was not initialized correctly"
+    assert initialObj.endValue == 0, "End value was not initialized correctly"
 
     # Get the arguments that have been passed in, and ignore the first one since it is the invocation of the script
     arguments = sys.argv[1:len(sys.argv)]
@@ -27,6 +31,9 @@ def setup():
         except ValueError:
             usage(2)
     
+    assert type(arguments[0]) is FloatType, "Argument one was not casted as a float"
+    assert type(arguments[1]) is FloatType, "Argument two was not casted as a float"
+    
     # Warn if arguments are not integers. Is there a fractional part.
     if not math.floor(arguments[0]) == arguments[0]:
         usage(3)
@@ -35,11 +42,17 @@ def setup():
 
     initialObj.startValue = int(arguments[0])
     initialObj.endValue = int(arguments[1])
-    
+
+    assert type(initialObj.startValue) is IntType, "Start value was not casted to an int correctly"
+    assert type(initialObj.endValue) is IntType, "End value was not casted to an int correctly"
+
     return initialObj
 
 # usage will be the initial error handling function so if the initial requirements are not met, print out the correct thing to do.
 def usage(errorCode):
+    
+    assert type(errorCode) is IntType, "Error code not recognized"
+    
     if(errorCode == 1):
         print 'ERROR - sequ requires exactly two integers'
         exit(1)
@@ -48,6 +61,8 @@ def usage(errorCode):
         exit(1)
     elif(errorCode == 3):
         print 'WARNING - Start and end arguments should be integers'
+    else:
+        print 'ERROR - An unexpected error has ocurred'
 
 # outputSeq will output the sequence between the integers passed in
 def outputSeq(sequObj):
@@ -58,7 +73,8 @@ def outputSeq(sequObj):
     if start > end:
         exit(1)
     for x in range(start, end + 1):
-        print x
+        outputStr = str(x)
+        print outputStr + sequObj.seperator,
     # The program was successful
     exit(0)
 
