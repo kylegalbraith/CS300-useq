3b1f879f84a4270279c1b78bf8db803cb942582c
Kyle Galbraith <kyle.galbraith459@gmail.com>
2013-11-01 22:15:58 -0700

    Parsing flags and numbers is functioning
    Got the base functionality for parsing the flags and then parsing the
    numbers. If you come across a number we break out of that loop as now
    only numbers will be accepted (3 numbers to be exact)

--- sequ.py
+++ sequ.py
@@ -23,37 +23,59 @@ def setup():
     totalArgs = len(arguments)
     stringParse = 0
 
-    while stringParse < totalArgs and isinstance(arguments[stringParse], basestring):
-        if arguments[stringParse] == "--help":
-            print "Go To Help"
-        if arguments[stringParse] == "--version":
-            print "Print version info"
-        if arguments[stringParse] == "--format" or arguments[stringParse] == "-f":
-            parameterLoop += 1
-            initialObj.formatOption = arguments[parameterLoop]
-            print "format" 
-        if arguments[stringParse] == "--seperator" or arguments[stringParse] == "-s":
-            print "sep for string"
-        if arguments[stringParse] == "--equal-width" or arguments[stringParse] == "-w":
-            print "set equal-width"     
-        parameterLoop += 1
-
-        
-    #if totalArgs > 2 or totalArgs == 1 or totalArgs == 0:
-     #   usage(1)
-
-    for x in range(totalArgs):
+    while stringParse < totalArgs:
+        # Try to cast each argument to a float, if it works then we break out of the loop as we have hit the 
+        # numerical portion of the args. Else continue parsing to see if the arg matches a flag.
         try:
-            arguments[x] = float(arguments[x])
+            arguments[stringParse] = float(arguments[stringParse])
+            break
         except ValueError:
-            usage(2)
+            if arguments[stringParse] == "--help":
+                usage(1)
+            if arguments[stringParse] == "--version":
+                usage(2)
+            if arguments[stringParse] == "--format" or arguments[stringParse] == "-f":
+                stringParse += 1
+                try:
+                    initialObj.formatOption = arguments[stringParse]
+                except IndexError:
+                    usage(4)
+                print initialObj.formatOption 
+            if arguments[stringParse] == "--seperator" or arguments[stringParse] == "-s":
+                print "sep for string"
+            if arguments[stringParse] == "--equal-width" or arguments[stringParse] == "-w":
+                print "set equal-width"     
+            stringParse += 1
     
-    assert type(arguments[0]) is FloatType, "Argument one was not casted as a float"
-    assert type(arguments[1]) is FloatType, "Argument two was not casted as a float"
+    totalNumberArgs = totalArgs - stringParse
+    numbers = []
+    assert totalNumberArgs >= 1, "There are no numbers in here"
     
-    initialObj.startValue = arguments[0]
-    initialObj.endValue = arguments[1]
- 
+    if(totalNumberArgs <= 3):
+        for x in range(stringParse, totalArgs):
+            try:
+                number = float(arguments[x])
+                numbers.append(number)
+            except ValueError:
+                usage(3)
+    else:
+        usage(3)
+
+    lengthOfNumbers = len(numbers)
+    assert lengthOfNumbers < 4, "Error in number assignments"
+
+    if(lengthOfNumbers == 3):
+        initialObj.startValue = numbers[0]
+        initialObj.step = numbers[1]
+        initialObj.endValue = numbers[2]
+    if(lengthOfNumbers == 2):
+        initialObj.startValue = numbers[0]
+        initialObj.endValue = numbers[1]
+    if(lengthOfNumbers == 1):
+        initialObj.endValue = numbers[0]
+
+    checkFormat = initialObj.formatOption % + initialObj.endValue
+  
     return initialObj
 
 # usage will be the initial error handling function so if the initial requirements are not met, print out the correct thing to do.
@@ -62,13 +84,17 @@ def usage(errorCode):
     assert type(errorCode) is IntType, "Error code not recognized"
     
     if(errorCode == 1):
-        print 'ERROR - sequ requires exactly two integers'
+        print 'Print help documentation'
         exit(1)
     elif(errorCode == 2):
-        print 'ERROR - sequ CLO only accepts integer types'
+        print 'Print version info'
         exit(1)
     elif(errorCode == 3):
-        print 'WARNING - Start and end arguments should be integers'
+        print 'sequ: extra operand'
+        exit(1)
+    elif(errorCode == 4):
+        print 'Invalid format string. Use --help for more information.'
+        exit(1)
     else:
         print 'ERROR - An unexpected error has ocurred'
 
