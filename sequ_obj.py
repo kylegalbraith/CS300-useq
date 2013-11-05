ce0b80029245dc84d4ceb7067e14019996b64124
Kyle Galbraith <kyle.galbraith459@gmail.com>
2013-11-04 16:13:18 -0800

    Changing default start value and equal width
    - Change default start value to 1
    - Change equal width to a bool

--- sequ_obj.py
+++ sequ_obj.py
@@ -4,13 +4,14 @@
 class sequ_obj:
     import sys
     """description of class"""
+    # set a global constant for default value of format
     # Constructor for sequ_obj
     def __init__(self):
-        self.startValue = 0
+        self.startValue = 1
         self.endValue = 0
         self.seperator = '\n'
         self.formatOption = "%g"
-        self.equalWidth = ""
+        self.equalWidth = False
         self.step = 1
         self.negativeStep = False
 
